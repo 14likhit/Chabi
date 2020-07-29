3ffd515ad3488ecc391e7a9abe3bf77974eaa735
likhit <likhit@udhaar.co>
2020-07-29 13:16:11 +0530

    Added tab layout in HomeActivity.kt. Updated the View.

--- app/src/main/java/com/likhit/chabi/ui/home/HomeActivity.kt
+++ app/src/main/java/com/likhit/chabi/ui/home/HomeActivity.kt
@@ -1,12 +1,18 @@
 package com.likhit.chabi.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.likhit.chabi.R
+import com.likhit.chabi.base.BaseActivity
+import com.likhit.chabi.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
+
+class HomeActivity : BaseActivity() {
+
+    private lateinit var activityHomeBinding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        activityHomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_home)
     }
 }
