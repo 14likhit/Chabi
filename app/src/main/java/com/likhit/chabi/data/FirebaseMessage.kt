6052a9cd367c1d929aced733767e4cb746047b88
likhit <likhit@udhaar.co>
2020-08-12 13:43:02 +0530

    [FirebaseMessage.kt]->Data Class for Firebase Messages. [MessageReceiverViewHolder.kt]->ViewHolder for Firebase Message reception. [MessageSenderViewHolder.kt]-> ViewHolder for Firebase Message sending.

---
+++ app/src/main/java/com/likhit/chabi/data/FirebaseMessage.kt
@@ -0,0 +1,7 @@
+package com.likhit.chabi.data
+
+/**
+ * Data Class for Firebase Messages
+ */
+data class FirebaseMessage(val id: String, val text: String, val name: String) {
+}
---
+++ app/src/main/java/com/likhit/chabi/ui/chat/MessageReceiverViewHolder.kt
@@ -0,0 +1,11 @@
+package com.likhit.chabi.ui.chat
+
+import androidx.recyclerview.widget.RecyclerView
+import com.likhit.chabi.databinding.LayoutMessageReceiverItemBinding
+
+/**
+ * ViewHolder for Firebase Message reception.
+ */
+class MessageReceiverViewHolder(val layoutMessageReceiverItemBinding: LayoutMessageReceiverItemBinding) :
+    RecyclerView.ViewHolder(layoutMessageReceiverItemBinding.root) {
+}
