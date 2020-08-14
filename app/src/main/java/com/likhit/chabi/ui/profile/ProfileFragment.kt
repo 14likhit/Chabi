a2a1a8d0625b4ed3ca63764a97bedddaacd19487
likhit <likhit@udhaar.co>
2020-08-14 15:46:56 +0530

    [ProfileFragment.kt]->Added logic to show profile if logged in. [fragment_profile.xml]->Updated Views. [layout_profile.xml]->Updated Views.

--- app/src/main/java/com/likhit/chabi/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/likhit/chabi/ui/profile/ProfileFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.likhit.chabi.R
 import com.likhit.chabi.base.BaseFragment
+import com.likhit.chabi.databinding.FragmentProfileBinding
+import com.likhit.chabi.utils.FirebaseAuthenticationHelper
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +26,8 @@ class ProfileFragment : BaseFragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var binding: FragmentProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -36,7 +41,38 @@ class ProfileFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun initViews(view: View) {
+        super.initViews(view)
+        setViewForUser()
+    }
+
+    private fun setViewForUser() {
+        if (FirebaseAuthenticationHelper.checkUserLoggedIn()) {
+            setUpChatView()
+        } else {
+            setUpSignInView()
+        }
+    }
+
+    private fun setUpSignInView() {
+        binding.signInButton.setOnClickListener {
+            FirebaseAuthenticationHelper.launchUserAuthenticationUI(this)
+        }
+        toggleView(false)
+    }
+
+    private fun setUpChatView() {
+        toggleView(true)
+    }
+
+    private fun toggleView(isUserLoggedIn: Boolean) {
+        binding.signInButton.visibility = if (isUserLoggedIn) View.GONE else View.VISIBLE
+        binding.profileLayout.profileNestedScrollView.visibility =
+            if (isUserLoggedIn) View.VISIBLE else View.GONE
     }
 
     companion object {
