c13408d140cf57608b5b65c94d147ed27f2f8672
likhit <likhit@udhaar.co>
2020-08-14 17:49:56 +0530

    [Utils.kt]->Added function to load image.

--- app/src/main/java/com/likhit/chabi/utils/Utils.kt
+++ app/src/main/java/com/likhit/chabi/utils/Utils.kt
@@ -1,6 +1,8 @@
 package com.likhit.chabi.utils
 
 import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 fun pxToDp(context: Context, px: Float): Float {
     return px / context.resources.displayMetrics.density
@@ -8,4 +10,19 @@ fun pxToDp(context: Context, px: Float): Float {
 
 fun dpToPx(context: Context, dp: Float): Float {
     return dp * context.resources.displayMetrics.density
+}
+
+fun loadImage(
+    context: Context?,
+    uri: String?,
+    placeholder: Int,
+    error: Int,
+    imageView: ImageView?
+) {
+    Glide.with(context!!)
+        .load(uri)
+        .placeholder(placeholder)
+        .error(error)
+        .centerCrop()
+        .into(imageView!!)
 }
